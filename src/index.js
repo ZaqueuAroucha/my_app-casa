6987633c7dddfe8a7199a8517897915e2b15ccb9
zaqueu aroucha da silva <zaqueuaroucha@gmail.com>
2022-09-30 17:14:49 -0300

    adicionando template defaut

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import { green, yellow } from '@material-ui/core/colors'
+
+
 import App from './App'
+import './index.css'
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: green [500],
+    },
+    secondary: {
+      main: yellow [500],
+    }
+  },
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/templates/Default.js
@@ -0,0 +1,28 @@
+import { colors } from '@material-ui/core'
+import { red } from '@material-ui/core/colors'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+
+import Header from "../partials/Header/Header"
+
+const useStyles = makeStyles(() => ({
+  container: {
+    padding: '15px 0',
+    background: 'red'
+  }
+}))
+
+const Default = ({ children }) => {
+  const classes = useStyles()
+
+  return (
+    <>
+      <Header />
+      <Container className={classes.container}>
+        {children}
+      </Container>
+    </>    
+  )
+}
+
+export default Default
